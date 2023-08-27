71962649c807f5754930cc92f0b27016b5db95f1
ruchirc64 <ruchirc@64-squares.com>
2023-08-27 16:54:18 +0530

    Initial commit

---
+++ BIE/Models/Model2.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def app():
+    st.title('Model2')
+
+    st.write('This is the `home page` of this multi-page app.')
+
+    st.write('In this app, we will be classification model using the Iris dataset.')
